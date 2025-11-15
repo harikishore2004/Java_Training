0aa9d501b60ebc76953638a106ab1dcac19f7a3d
M Hari Kishore <mharikishore.work@gmail.com>
2025-11-15 13:09:05 +0530

    Implemented Method Overiding

---
+++ src/in/cemk/training/MyCar.java
@@ -0,0 +1,14 @@
+package in.cemk.training;
+
+public class MyCar extends Suzuki{
+	
+	public void car() {
+		System.out.println("This the over rided method of the Suzuki class");
+	}
+	public static void main(String[] args) {
+		MyCar myCar = new MyCar();
+		myCar.car();
+
+	}
+
+}
---
+++ src/in/cemk/training/Suzuki.java
@@ -0,0 +1,7 @@
+package in.cemk.training;
+
+public class Suzuki {
+	public void car() {
+		System.out.println("Suzuki is a company from Japan");
+	}
+}
